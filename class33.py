80708699fba197b6aa5052402caaf4fd30d3c302
Adiyan <160841125+myzuhh1@users.noreply.github.com>
2026-05-23 21:45:12 +0600

    Add files via upload

---
+++ class33.py
@@ -0,0 +1,35 @@
+print("hello world")
+print("2") # print of 2
+print("im 14") # adiyan is 14
+print("4") # print of 4
+print("5") # print of 5
+
+# class 33 part 2
+
+# print a sentence
+print("this is a sentence.")
+
+#print a number
+print("1234")
+
+# next line after print
+print("hello python \n")
+
+#print all the keywords present in python
+print("all the keywords present in python are: ")
+import keyword
+print("keyword.kwlist")
+
+# class 34 part 3
+
+# greeting msg
+print("hello world!")
+
+# my name
+print("i am adiyan")
+
+# fun fact about myself
+print("i love playing soccer")
+
+# simple calculation
+print("the result of 3+5 is:", 3 + 5)
